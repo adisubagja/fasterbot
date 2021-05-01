27f2f0a2ac24d0d544dd4c8b7a7af0bfff096f60
bro <upinfour4@gmail.com>
2021-05-02 01:23:13 +0700

    simplify payment channels

--- checkoutdata.py
+++ checkoutdata.py
@@ -1,41 +1,53 @@
-from dataclasses import dataclass
 from enum import Enum
+from typing import Dict
 
 
-class PaymentChannel(Enum):
-    ALFAMART = 8003200
-    INDOMART_ISAKU = 8003001
-    AKULAKU = 8000700
-    TRANSFER_BANK = 8005200
-    COD_BAYAR_DI_TEMPAT = 89000
-    SHOPEE_PAY = 8001400
+class PaymentChannel:
+    __name: str
+    __channel_id: int
+    __options: Dict[str, str]
+    __version: int
 
-    @property
+    def __init__(self, name: str, channel_id: int, options: Dict[str, str] = None, version: int = 2):
+        self.__name = name
+        self.__channel_id = channel_id
+        self.__options = options
+        self.__version = version
+
+    def name(self) -> str:
+        return self.__name
+
+    def channel_id(self) -> int:
+        return self.__channel_id
+
+    def options(self) -> Dict[str, str]:
+        return self.__options
+
     def version(self) -> int:
-        return 1 if self is PaymentChannel.COD_BAYAR_DI_TEMPAT else 2
+        return self.__version
+
+    def has_option(self) -> bool:
+        return self.__options is not None
 
 
-class PaymentChannelOptionInfo(Enum):
-    NONE = ""
-    # Transfer Bank
-    TRANSFER_BANK_BCA_AUTO = "89052001"
-    TRANSFER_BANK_MANDIRI_AUTO = "89052002"
-    TRANSFER_BANK_BNI_AUTO = "89052003"
-    TRANSFER_BANK_BRI_AUTO = "89052004"
-    TRANSFER_BANK_SYARIAH_AUTO = "89052005"
-    TRANSFER_BANK_PERMATA_AUTO = "89052006"
-
-    # Akulaku
-    AKULAKU_CICILAN_1X = "8000700-25"
-    AKULAKU_CICILAN_2X = "8000700-26"
-    # untested
-    AKULAKU_CICILAN_3X = "8000700-27"
-    AKULAKU_CICILAN_6X = "8000700-28"
-    AKULAKU_CICILAN_9X = "8000700-29"
-    AKULAKU_CICILAN_12X = "8000700-30"
-
-
-@dataclass
-class PaymentInfo:
-    channel: PaymentChannel
-    option_info: PaymentChannelOptionInfo = PaymentChannelOptionInfo.NONE
+class PaymentChannelList(Enum):
+    ALFAMART = PaymentChannel("Alfamart", 8003200)
+    INDOMART = PaymentChannel("Indomart iSaku", 8003001)
+    AKULAKU = PaymentChannel("Akulaku", 8000700, {
+        "Akulaku Cicilan 1x": "8000700-25",
+        "Akulaku Cicilan 2x": "8000700-26",
+        "Akulaku Cicilan 3x": "8000700-27",
+        "Akulaku Cicilan 6x": "8000700-28",
+        "Akulaku Cicilan 9x": "8000700-29",
+        "Akulaku Cicilan 12x": "8000700-30"
+    })
+    TRANSFER_BANK = PaymentChannel("Transfer Bank", 8005200, {
+        "Transfer Bank BCA (Dicek Otomatis)": "89052001",
+        "Transfer Bank Mandiri (Dicek Otomatis)": "89052002",
+        "Transfer Bank BNI (Dicek Otomatis)": "89052003",
+        "Transfer Bank BRI (Dicek Otomatis)": "89052004",
+        "Transfer Bank Syariah (Dicek Otomatis)": "89052005",
+        "Transfer Bank Permata (Dicek Otomatis)": "89052006"
+    })
+    COD = PaymentChannel("COD (Bayar di Tempat)", 89000, version=1)
+    SHOPEE_PAY = PaymentChannel("Shopee Pay", 8001400)
